0d504c90b0822aada34232990e0ef0dc59bbd33b
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2021-08-07 15:06:25 -0600

    [NUEVO] Metodos
    Se agregan metodos nuevos

--- REST-API/src/routes/spaces.js
+++ REST-API/src/routes/spaces.js
@@ -2,18 +2,55 @@ const { Router, request } = require('express');
 const router = Router();
 const _ = require('underscore');
 
-const spaces = require('../sample.json');
+const spaces = require('../spaces.json');
+const vehicles = require('../vehicles.json');
 
 router.get('/spaces', (req, res) => {
-    res.json(spaces);
+    const { state } = req.query;
+    if (state) {
+        if ((state === 'free' || state === 'in-use')) {
+            let result = [];
+            _.each(spaces, (space, i) => {
+                if (space.state == state) {
+                    result.push(space);
+                }
+            });
+            res.json(result);
+        } else {
+            res.status(404).json({ error: 'State invalido' });
+        }
+        
+    } else {
+        res.json(spaces);
+    }
+});
+
+router.get('/spaces/:id', (req, res) => {
+    const { id } = req.params;
+    let result = '';
+
+    for (let i = 0; i < spaces.length; i++) {
+        if (spaces[i].id == id) {
+            result = spaces[i];
+            break;
+        }
+    }
+
+    if(result != ''){
+        res.json(result);
+    } else {
+        res.status(404).json({error: 'No se encuentra el elemento especificado'});
+    }
+    
 });
 
 router.post('/spaces', (req, res) => {
     console.log(req.body);
-    const {state} = req.body;
-    if(state){
-        const id = spaces.length + 1
-        let newSpace = {...req.body, id};
+    const { info } = req.body;
+    if(info){
+        let id = (spaces.length + 1).toString();
+        let state = "free";
+        let newSpace = {...req.body, id, state};
         spaces.push(newSpace);
         res.json(spaces);
     } else {
@@ -23,11 +60,11 @@ router.post('/spaces', (req, res) => {
 
 router.put('/spaces/:id', (req, res) => {
     const { id } = req.params;
-    const {state} = req.body;
-    if(state){
+    const {info} = req.body;
+    if(info){
         _.each(spaces, (space, i) => {
             if(space.id == id){
-                space.state = state; //se actualizan todos los atributos
+                space.info = info; //se actualizan todos los atributos
             }
         });
         res.json(spaces);
@@ -39,12 +76,34 @@ router.put('/spaces/:id', (req, res) => {
 
 router.delete('/spaces/:id', (req, res) => {
     const { id } = req.params;
-    _.each(spaces, (space, i) => {
-        if (space.id == id ){
-            spaces.splice(i, 1);
+    let reservado = false;
+    let indice;
+    for (let i = 0; i < spaces.length; i++) {
+        if (spaces[i].id == id) {
+            indice = i;
+            for (let j = 0; j < vehicles.length; j++) {
+                if (vehicles[j].spaceId == id) {
+                    reservado = true;
+                    break;
+                }
+            }
+            break;
         }
-    });
-    res.json(spaces);
+    }
+
+    if(indice !== null){
+        if(!reservado){
+            spaces.splice(indice, 1);
+            res.json(spaces);
+        } else {
+            res.status(404).json({error: 'Hay un vehiculo en ese espacio'});
+        }
+    }
+    else {
+        res.status(404).json({error: 'No se encontro el espacio a eliminar'});
+    }
+   
 });
 
 module.exports = router;
+
